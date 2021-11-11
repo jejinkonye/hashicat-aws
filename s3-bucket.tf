0cacc1c02359c7c54e10c22ed945ddae02fcc246
Jude Ejinkonye <jude.ejinkonye@digitalonus.com>
2021-11-11 21:22:02 +0000

    Removed bucket name for bucket prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
-  bucket = "gaurav-s3-bucket"
+ #  bucket = "gaurav-s3-bucket"
   acl    = "private"
   bucket_prefix = var.prefix
   versioning = {
